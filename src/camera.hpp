819d44cb882548e06da725e88997eaf706189585
c-devhax <sarcasticdiscussions2@gmail.com>
2021-02-03 10:41:23 +0200

    Moving camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -4,20 +4,24 @@
 
 class camera {
 public:
-	camera() {
-		double aspect_ratio = 16.0 / 9.0;
-		double viewport_height = 2.0;
-		double viewport_width = viewport_height * aspect_ratio;
-		double focal_length = 1.0;
+	camera(point3 lookfrom, point3 lookat, vec3 vup, double vert_fov, double aspect_ratio) {
+		auto theta = degrees_to_radians(vert_fov);
+		double h = tan(theta / 2);
+		double viewport_height = 2.0 * h;
+		double viewport_width = aspect_ratio * viewport_height;
 
-		origin = point3(0.0, 0.0, 0.0);
-		horizontal = vec3(viewport_width, 0.0, 0.0);
-		vertical = vec3(0.0, viewport_height, 0.0);
-		lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0.0, 0.0, focal_length);
+		vec3 w = unit_vector(lookfrom - lookat);
+		vec3 u = unit_vector(cross(vup, w));
+		vec3 v = cross(w, u);
+
+		origin = lookfrom;
+		horizontal = viewport_width * u;
+		vertical = viewport_height * v;
+		lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
 	}
 
-	ray get_ray(double u, double v) const {
-		return ray(origin, lower_left_corner + u * horizontal+ v * vertical - origin);
+	ray get_ray(double s, double t) const {
+		return ray(origin, lower_left_corner + s * horizontal + t * vertical- origin);
 	}
 
 private:

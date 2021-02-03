534540140576a6baab53f7c71d32dd66fe4e2505
c-devhax <sarcasticdiscussions2@gmail.com>
2021-02-03 10:09:47 +0200

    Glass spheres and Dielectrics

--- src/material.hpp
+++ src/material.hpp
@@ -43,3 +43,40 @@ private:
 	color albedo;
 	double fuzziness;
 };
+
+class dielectric : public material {
+public:
+	dielectric(double ir) : ir(ir) {}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+
+		double cos_theta = fmin(dot(-unit_direction, rec.normal), -1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+		bool can_refract = refraction_ratio * sin_theta <= 1.0;
+		vec3 direction;
+
+		if (!can_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+			direction = reflect(unit_direction, rec.normal);
+		else
+			direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+		scattered = ray(rec.p, direction);
+		return true;
+	}
+
+private:
+	double ir; // index of refraction
+
+private:
+	static double reflectance(double cos, double ref_idx) {
+		// Christophe Schlick's approx.
+		double r0 = (1 - ref_idx) / (1 + ref_idx);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * pow(1 - cos, 5);
+	}
+};
